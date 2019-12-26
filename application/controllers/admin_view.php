c1e742b6a1d53678a65737c712c4d84c23fc95d0
Trian Damai <triannurizkillah@gmail.com>
2019-12-26 12:13:02 +0700

    inisalisasi_projek

---
+++ application/controllers/admin_view.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class admin_view extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+    }
+
+   
+    
+}
