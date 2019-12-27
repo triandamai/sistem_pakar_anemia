f1081d11cf8d174999831efa92091a502cf1695c
Trian Damai <triannurizkillah@gmail.com>
2019-12-28 00:07:28 +0700

    build-up

---
+++ application/views/admin/tambah-gejala.php
@@ -0,0 +1,28 @@
+ <!-- Main Content -->
+ <div class="main-content">
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="section-header">
+                    <h1><?= $nama_section;?></h1>
+                    <div class="section-header-breadcrumb">
+                        <div class="breadcrumb-item active"><a href="<?= base_url()?>index.php/admin_view/">Home</a></div>
+                        <div class="breadcrumb-item"><?= $nama_section;?></div>
+                    </div>
+                </div>
+                <div class="section-body">
+                    <h2 class="section-title"><?= $title_section;?></h2>
+                    <p class="section-lead"><?= $subtitle_section;?></p>
+                    <div class="row">
+                    <div class="col-12 col-md-12 col-lg-12">
+                        <div class="card">
+                        <div class="card-header">
+                            <h4>Full Width</h4>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
