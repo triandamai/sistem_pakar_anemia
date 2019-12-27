139c09836cb722c4e1df4ee4a60b1bfdfb2891dc
Trian Damai <triannurizkillah@gmail.com>
2019-12-27 23:32:00 +0700

    build-up

--- application/controllers/admin_view.php
+++ application/controllers/admin_view.php
@@ -5,6 +5,10 @@ class admin_view extends CI_Controller {
 
 	public function index()
 	{
+       
+    }
+    public function admin_login()
+	{
         $data['title'] = "Admin | Login";
         $this->load->view('header',$data);
             $this->load->view('admin/auth-login',$data);
@@ -19,5 +23,100 @@ class admin_view extends CI_Controller {
             $this->load->view('admin/side-nav-bottom',$data);
         $this->load->view('footer',$data);
     }
+
+    public function admin_data_penyakit(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/data-penyakit',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+    public function admin_tambah_penyakit(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/tambah-penyakit',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+
+    public function admin_data_gejala(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/data-gejala',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+    public function admin_tambah_gejala(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/tambah-gejala',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+
+    public function admin_data_saran_penyakit(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/data-saran-penyakit',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+    public function admin_tambah_saran_penyakit(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/tambah-saran-penyakit',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+
+    public function admin_data_user(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/data-user',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+    public function admin_tambah_user(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/tambah-user',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+    public function admin_history_user(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/history-user',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+
+    public function admin_data_artikel(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/data-artikel',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+    public function admin_tambah_artikel(){
+        $data['title'] = "Admin | Penyakit";
+        $this->load->view('header',$data);
+            $this->load->view('admin/side-nav-top',$data);
+            $this->load->view('admin/tambah-artikel',$data);
+            $this->load->view('admin/side-nav-bottom',$data);
+        $this->load->view('footer',$data);
+    }
+
+
     
 }
--- application/views/admin/side-nav-top.php
+++ application/views/admin/side-nav-top.php
@@ -33,39 +33,39 @@
             <a href="index.html">A</a>
           </div>
           <ul class="sidebar-menu">     
-              <li><a class="nav-link" href="#"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+              <li><a class="nav-link" href="<?= base_url()?>"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
               <li class="menu-header">Penyakit</li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Gejala</span></a>
-                <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="#">Data Gejala</a></li>
-                  <li><a class="nav-link" href="#">Tambah Gejala</a></li>
-                </ul>
-              </li>
               <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Penyakit</span></a>
                 <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="#">Data Penyakit</a></li>
-                  <li><a class="nav-link" href="#">Tambah Penyakit</a></li>
-                  <li><a class="nav-link" href="#">Data Saran Penyakit</a></li>
-                  <li><a class="nav-link" href="#">Tambah Saran Penyakit</a></li>
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_data_penyakit">Data Penyakit</a></li>
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_tambah_penyakit">Tambah Penyakit</a></li>
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_data_saran_penyakit">Data Saran Penyakit</a></li>
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_tambah_saran_penyakit">Tambah Saran Penyakit</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Gejala</span></a>
+                <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_data_gejala">Data Gejala</a></li>
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_tambah_gejala">Tambah Gejala</a></li>
                 </ul>
               </li>
               <li class="menu-header">Artikel</li>
               <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Artikel</span></a>
                 <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="components-article.html">Data Artikel</a></li>
-                  <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Tambah Artikel</a></li>
+                  <li><a class="nav-link" href="<?= base_url()?>index.php/admin_view/admin_data_artikel">Data Artikel</a></li>
+                  <li><a class="nav-link beep beep-sidebar" href="<?= base_url()?>index.php/admin_view/admin_tambah_artikel">Tambah Artikel</a></li>
                 </ul>
               </li>
               <li class="menu-header">User</li>
               <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>User</span></a>
                 <ul class="dropdown-menu">
-                  <li><a href="#">Data User</a></li>
-                  <li><a href="#">Tambah User</a></li>
-                  <li><a class="beep beep-sidebar" href="#">History</a></li>
+                  <li><a href="<?= base_url()?>index.php/admin_view/admin_data_user">Data User</a></li>
+                  <li><a href="<?= base_url()?>index.php/admin_view/admin_tambah_user">Tambah User</a></li>
+                  <li><a class="beep beep-sidebar" href="<?= base_url()?>index.php/admin_view/admin_history_user">History</a></li>
                 </ul>
               </li>
              
